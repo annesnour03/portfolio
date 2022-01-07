d47a7f2366b21cb339a1597a119ee38d80c54a13
Annes Negmel-Din <annes.negmel-din@student.uva.nl>
2022-01-07 18:22:29 +0100

    empty input detect

--- logic.js
+++ logic.js
@@ -1,11 +1,14 @@
 function calculate(){
-    var firstname =  document.getElementById("firstname").val
-    console.log("mieble")
+    var firstname =  document.getElementById("firstname").value
+    var secondname =  document.getElementById("secondname").value
+    if(firstname == "" || secondname ==""){
+        console.log("empty input")
+        return
+    }
     document.getElementById('calculate').innerHTML = "Calculating..."
 
     setTimeout(() => {
         document.getElementById('calculate').innerHTML = "Calculate"
     }, 2000);
 
-    alert(firstname) // Werkt niet voor een reden ?? is undefined
 }
