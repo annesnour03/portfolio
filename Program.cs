782ed1c81bc154b7421486dc10090dee2dff50eb
Annes Negmel-Din <annes.negmel-din@student.uva.nl>
2022-06-04 19:11:52 +0200

    reset, going for a retry

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using personal_website;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
