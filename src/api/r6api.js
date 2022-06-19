dc77fb8e13f5a90e722a8003379d6a4a75907e51
Annes Negmel-Din <annes.negmel-din@student.uva.nl>
2022-06-19 09:59:59 +0200

    r6 command basics

--- src/api/r6api.js
+++ src/api/r6api.js
@@ -1,37 +1,51 @@
+import axios from "axios"
 const cheerio = require("cheerio")
-const axios = require('axios').default;
+
 
 const api = {
     userInfo: userInfo
 }
+function getText(struc) {
+    return struc.first().text().trim()
+}
+
+function toInt(string){
+    return parseInt(string.replace(',',''))
+}
+
+async function userInfo(uname, platform) {
+    const { data } = await axios.get(`https://corsanywhere.herokuapp.com/https://r6.tracker.network/profile/${platform}/${uname}`)
+    const $ = await cheerio.load(data)
+    const username = getText($(".trn-profile-header__name"))
+    const level = getText($(".trn-card__content  > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2)"))
+    const currentMMR = toInt(getText($("#profile > div.trn-scont.trn-scont--swap > div.trn-scont__aside > div:nth-child(1) > div.trn-card__content.trn-card--light.pt8.pb8 > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2)")))
 
 
-async function userInfo(username, platform) {
+    const wins = toInt(getText($("div.trn-defstat--large:nth-child(1) > div:nth-child(2)")))
+    const winPercentage = getText($("div.trn-defstat--large:nth-child(2) > div:nth-child(2)"))
+    const kills = toInt(getText($("div.trn-defstat--large:nth-child(3) > div:nth-child(2)")))
+    const kd = getText($("div.trn-defstat--large:nth-child(4) > div:nth-child(2)"))
 
-    // fetch('https://r6.tracker.network/profile/pc/xruprim/',{mode:'no-cors'})
-    fetch('https://r6.tracker.network/profile/pc/xruprim').then((response) => {
-            return response
-        }).then((d) => {
-            console.log('parsed json', d)
+    const timePlayed = getText($("#profile > div.trn-scont.trn-scont--swap > div.trn-scont__content > div:nth-child(2) > div.trn-card__content > div.trn-defstats.trn-defstats--width4 > div:nth-child(6) > div.trn-defstat__value"))
+    const bestAllTime = toInt(getText($("#profile > div.trn-scont.trn-scont--swap > div.trn-scont__aside > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)")).slice(0, -4))
 
-        }).catch(function (ex) {
-            console.log('parsing failed', ex)
+    const info = {
+        wins,
+        winPercentage,
+        kills,
+        kd,
+        // 
+        // 
+        username,
+        level,
+        currentMMR,
+        bestAllTime,
+        timePlayed,
+    }
 
-        })
-
-
-// fetch("https://r6.tracker.network/profile/pc/xruprim", {
-//     mode: 'no-cors',
-//     method: 'get',
-//     headers: {
-//         accept: 'application/json',
-//     }
-
-// }).then((body) => {
-//     console.log(body);
-// })
-return username
-
+    return info
 }
 
 export default api
+
+
--- src/components/TermExecute.js
+++ src/components/TermExecute.js
@@ -2,79 +2,158 @@ import { asciiAnnes, availableCommands, futureCommands } from "../assets/jsxElem
 import './../index.css';
 import api from '../api/r6api'
 
-    function run(props, args, setReadOnly, setOutputMessage) {
-        const command = args[0]
-        const commandHandler = {
-            "help": help,
-            "welcome": welcome,
-            "whoami": whoami,
-            "clear": clear,
-            "r6": r6,
-            "": enter
-        }[command]
-        commandHandler?.()
+function run(props, args, setReadOnly, setOutputMessage) {
+    const command = args[0]
+    const commandHandler = {
+        "help": help,
+        "welcome": welcome,
+        "whoami": whoami,
+        "clear": clear,
+        "r6": r6,
+        "": enter
+    }[command]
+    commandHandler?.()
 
-        function help() {
-            setReadOnly(true)
-            setOutputMessage(<>
+    function help() {
+        setReadOnly(true)
+        setOutputMessage(<>
 
-                <p>{"Currently available commands:"}</p>
-                <div className="margin-0" style={{ "color": "green" }}>
+            <p>{"Currently available commands:"}</p>
+            <div className="margin-0" style={{ "color": "green" }}>
 
-                    {availableCommands}
-                </div>
-                <p>{"Commands that will be avaiable in the future:"}</p>
-                {futureCommands}
-            </>
-            )
-        }
-
-        function welcome() {
-            setReadOnly(true)
-            setOutputMessage(<>
-                {asciiAnnes}
-                <br></br>
-                <p>
-                    Hello my name is <a style={{ color: "#88f0fd" }}>Annes Negmel-Din</a> and this is my interactive portfolio.
-                </p>
-                <p>
-                    If you want to switch to GUI mode, enter "gui" or click on the floating button.
-                </p>
-                <p>
-                    To see all the commands, try "help"!
-                </p>
-            </>
-            )
-        }
-
-        function whoami() {
-            const today = new Date()
-            // American style date.
-            const birthday = new Date("03-01-2003")
-
-            // Divide by milliseconds per day
-            const daysSinceBirth = Math.floor((today - birthday) / (1000 * 60 * 60 * 24))
-            const age = Math.floor(daysSinceBirth / 365.25)
-            const days = Math.floor(daysSinceBirth % 365.25)
-            setOutputMessage(
-                <p>
-                    My name is <a style={{ color: "#88f0fd" }}>Annes Negmel-Din</a> and I'm {age} years and {days} days old.
-                </p>)
-            setReadOnly(true)
-        }
-
-        function clear() {
-            props.clear()
-        }
-
-        function enter() {
-            setReadOnly(true)
-        }
-        
-        function r6() {
-            console.log(api.userInfo("hi","hello"));
-            setReadOnly(true)
-        }
+                {availableCommands}
+            </div>
+            <p>{"Commands that will be avaiable in the future:"}</p>
+            {futureCommands}
+        </>
+        )
     }
+
+    function welcome() {
+        setReadOnly(true)
+        setOutputMessage(<>
+            {asciiAnnes}
+            <br></br>
+            <p>
+                Hello my name is <a style={{ color: "#88f0fd" }}>Annes Negmel-Din</a> and this is my interactive portfolio.
+            </p>
+            <p>
+                If you want to switch to GUI mode, enter "gui" or click on the floating button.
+            </p>
+            <p>
+                To see all the commands, try "help"!
+            </p>
+        </>
+        )
+    }
+
+    function whoami() {
+        const today = new Date()
+        // American style date.
+        const birthday = new Date("03-01-2003")
+
+        // Divide by milliseconds per day
+        const daysSinceBirth = Math.floor((today - birthday) / (1000 * 60 * 60 * 24))
+        const age = Math.floor(daysSinceBirth / 365.25)
+        const days = Math.floor(daysSinceBirth % 365.25)
+        setOutputMessage(
+            <p>
+                My name is <a style={{ color: "#88f0fd" }}>Annes Negmel-Din</a> and I'm {age} years and {days} days old.
+            </p>)
+        setReadOnly(true)
+    }
+
+    function clear() {
+        props.clear()
+    }
+
+    function enter() {
+        setReadOnly(true)
+    }
+
+    async function r6() {
+        //  Simply r6, so display the squad's stats
+        if (!args[1]) {
+            setOutputMessage(
+                <span>
+                    Loading the squad's data! Please hold.
+                </span>)
+
+            Promise.all(await fetchAllPlayers(["xruprim", "hockeyHeld", "PhantomGod73", "IIJulianll", "Widder7013", "Koen_Meneer"])).
+                then(
+                    (players) => {
+                        players.sort((first, second) => {
+                            return second.currentMMR - first.currentMMR
+                        }
+                        )
+                        return players
+                    }).then((sorted) => {
+                        setOutputMessage(
+                            sorted.map((player, index) => {
+                                // Try to make the "═" the same length as others.
+                                const fillLength = Math.ceil((60 - player.username.length) / 2)
+                                return (
+                                    <>
+
+                                        <span key={index}>
+                                            {`${"═".repeat(fillLength)}${player.username}${"═".repeat(fillLength)}`}
+                                        </span>
+                                        <br />
+                                        {/* Here we display the stats */}
+                                        <div className="stats">
+                                            current mmr:
+                                            <span style={{ "color":"#6cf03c"}}>
+                                                 {player.currentMMR}
+
+                                            </span>
+                                            {/* <br /> */}
+                                            <span>
+                                                | wins: {player.wins} |
+
+                                            </span>
+                                            <span>
+                                                time played: {player.timePlayed}
+                                            </span>
+                                            <br />
+                                            <br />
+                                        </div>
+                                            <br />
+
+                                    </>
+                                )
+                            })
+
+                        )
+
+                    })
+
+        } else {
+            api.userInfo("xruprim", "pc").then((userInfo) => {
+                console.log(userInfo);
+                setOutputMessage(
+                    <span>
+                        ══════════════════════{userInfo.username}══════════════════════
+                    </span>)
+
+            })
+
+        }
+
+        setReadOnly(true)
+    }
+    async function fetchAllPlayers(players) {
+        let promises = []
+        players.forEach(player => {
+            const promise = api.userInfo(player, "pc")
+            promises.push(promise)
+        });
+        return promises
+        // const yo = await api.userInfo("xruprim", "pc")
+        // const yo1 = await api.userInfo("hockeyheld", "pc")
+        // const yo2 = await api.userInfo("PhantomGod73", "pc")
+        // const yo3 = await api.userInfo("IIJulianll", "pc")
+        // return [yo,yo1,yo2,yo3]
+    }
+}
 
 export default run
