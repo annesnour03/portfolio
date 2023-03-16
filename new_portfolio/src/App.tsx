60264a57258b2df23c041cbb70512ac458c13777
Annes Negmel-Din <annesnour03@gmail.com>
2023-03-16 22:14:26 +0100

    Initialise new portfolio frontend

---
+++ new_portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
+
+      <h2 className="text-8xl text-yellow-">test</h2>
+    </>
+  );
+}
+
+export default App;
