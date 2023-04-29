463328dff4d5f187d42b749bf3b7368470c74998
Annes Negmel-Din <annesnour03@gmail.com>
2023-04-29 17:32:13 +0200

    WIP terminal history

--- new_portfolio/src/components/terminal/TerminalInputPromptLine.tsx
+++ new_portfolio/src/components/terminal/TerminalInputPromptLine.tsx
@@ -3,6 +3,7 @@ import {
   RefObject,
   SetStateAction,
   SyntheticEvent,
+  useEffect,
   useRef,
   useState,
 } from "react";
@@ -36,14 +37,33 @@ export const TerminalInputPromptLine = ({
   setHistory: Dispatch<SetStateAction<TerminalHistory[]>>;
   inputRef: RefObject<HTMLInputElement>;
 }) => {
+  const [allPrompts, setAllPrompts] = useState<string[]>([""]);
+  const [historyIndex, setHistoryIndex] = useState<number>(
+    allPrompts.length - 1
+  );
+  const getLatestPrompt = (): string => allPrompts.at(-1) ?? "";
+  const updateLatestPrompt = (newValue: string) => {
+    setAllPrompts([...allPrompts.slice(0, -1), newValue]);
+  };
+  const pushNewPrompt = (newValue: string) => {
+    setAllPrompts([...allPrompts, newValue]);
+  };
   const handleOnChange = (e: SyntheticEvent) => {
     const target = e.target as HTMLInputElement;
-    setPrompt(target.value);
+    updateLatestPrompt(target.value);
+  };
+
+  const getCommandHistoryData = () => {
+    const removeAdjacentDuplicates = (arr: string[]): string[] => {
+      return arr.filter((elem, i) => i === 0 || elem !== arr[i - 1]);
+    };
+    return removeAdjacentDuplicates(allPrompts).filter((elem) => elem !== "");
   };
 
   const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const key = e.key;
 
+    const latestPrompt = getLatestPrompt();
     // Handle submission
     if (key === "Enter") {
       e.preventDefault();
@@ -51,12 +71,15 @@ export const TerminalInputPromptLine = ({
       setHistory([
         ...history,
         {
-          command: prompt,
-          commandValid: commandExists(prompt),
-          output: runCommand(prompt),
+          command: latestPrompt,
+          commandValid: commandExists(latestPrompt),
+          output: runCommand(latestPrompt),
         },
       ]);
-      setPrompt("");
+      setHistoryIndex(getCommandHistoryData().length);
+      pushNewPrompt(latestPrompt);
+      pushNewPrompt("");
+
       // After we have inserted the history, we scroll smoothly to it.
       scrollSmoothlyToBottom("term");
     } else if (key === "Tab") {
@@ -66,18 +89,41 @@ export const TerminalInputPromptLine = ({
       setHistory([
         ...history,
         {
-          command: prompt,
-          commandValid: commandExists(prompt),
+          command: latestPrompt,
+          commandValid: commandExists(latestPrompt),
           output: <></>,
         },
       ]);
-      setPrompt("");
+      updateLatestPrompt("");
       // After we have inserted the history, we scroll smoothly to it.
       scrollSmoothlyToBottom("term");
+    } else if (key === "ArrowUp") {
+      e.preventDefault();
+      // We have reached the end of the history
+      if (historyIndex <= 0) {
+        return;
+      }
+      console.log(
+        "yooyoyoyoy",
+        allPrompts,
+        historyIndex,
+        allPrompts[historyIndex - 1]
+      );
+      updateLatestPrompt(getCommandHistoryData()[historyIndex - 1]);
+      setHistoryIndex(historyIndex - 1);
+    } else if (key === "ArrowDown") {
+      e.preventDefault();
+
+      console.log("getting past", getCommandHistoryData(), historyIndex);
+      if (historyIndex === getCommandHistoryData().length - 1) {
+        updateLatestPrompt("  ");
+        setHistoryIndex(historyIndex);
+        return;
+      }
+      updateLatestPrompt(getCommandHistoryData()[historyIndex + 1]);
+      setHistoryIndex(historyIndex + 1);
     }
   };
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [prompt, setPrompt] = useState<string>("");
   return (
     <>
       <span className="flex flex-row items-center">
@@ -91,10 +137,10 @@ export const TerminalInputPromptLine = ({
           className=""
           id="latest-input"
           key="enterInput"
-          value={prompt}
+          value={getLatestPrompt()}
           onChange={handleOnChange}
           onKeyDown={handleKey}
-          $commandValid={commandExists(prompt)}
+          $commandValid={commandExists(getLatestPrompt())}
           onBlur={({ target }) => target.focus()}
           autoFocus
         />
