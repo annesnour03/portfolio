bb4aa3d9f6e69f9597bfe2c776cbba3a972857d9
Annes Negmel-Din <annesnour03@gmail.com>
2023-05-03 16:43:43 +0200

    Working on commands

--- new_portfolio/src/components/terminal/bin/general.tsx
+++ new_portfolio/src/components/terminal/bin/general.tsx
@@ -1,10 +1,11 @@
-import { GITHUB_SOURCE_URL, LINKEDIN_URL } from "appconstants";
 import * as bin from "./index";
+import {
+  GITHUB_SOURCE_URL,
+  LINKEDIN_URL,
+  asciiAnnes,
+  myAge,
+} from "appconstants";
 import { openLink } from "helpers/General";
-
-export const test = () => {
-  return <>inbin</>;
-};
 
 export const help = () => {
   const allCommands: string[] = Object.keys(bin);
@@ -20,21 +21,42 @@ export const help = () => {
 
 export const welcome = () => {
   return (
-    <p>
-      Welcome, my name is
-      <a
-        href={LINKEDIN_URL}
-        target="_blank"
-        rel="noreferrer"
-        className="text-cyan-300"
-      >
-        Annes Negmel-Din
-      </a>
-    </p>
+    <div>
+      {asciiAnnes}
+      <p>
+        Welcome, my name is{" "}
+        <a
+          href={LINKEDIN_URL}
+          target="_blank"
+          rel="noreferrer"
+          className="text-cyan-300"
+        >
+          Annes Negmel-Din
+        </a>
+        . I am a {myAge}-year-old Computer Science student and this is my
+        interactive terminal portfolio.
+      </p>
+      <p>
+        To get started with some commands, type "
+        <p className="inline-block text-cyan-300">help</p>".
+      </p>
+      <p>
+        You can go back into your command history with the{" "}
+        <p className="inline-block text-cyan-300">UP/DOWN</p> arrows.
+      </p>
+      <p>
+        You can also accept a suggestion by simply pressing the{" "}
+        <p className="inline-block text-cyan-300">TAB</p> key.
+      </p>
+    </div>
   );
 };
 
 export const src = () => {
   openLink(GITHUB_SOURCE_URL);
   return <></>;
+};
+
+export const whoami = () => {
+  return <>guest</>;
 };
