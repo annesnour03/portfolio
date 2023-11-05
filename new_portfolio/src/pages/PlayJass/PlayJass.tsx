93774a5f2614c23562a6bfab1cd86ba950e8290d
Annes Negmel-Din <annesnour03@gmail.com>
2023-11-05 20:24:39 +0100

    fix(jass): title & couleBeMistake logic

--- new_portfolio/src/pages/PlayJass/PlayJass.tsx
+++ new_portfolio/src/pages/PlayJass/PlayJass.tsx
@@ -46,7 +46,7 @@ const ConfirmGoingWetPopOver = ({
   if (selectedId === null) return <></>;
   const teamName = game[selectedId].teamName;
   const couldBeMistake =
-    calculatePoints(game[selectedId]) < calculatePoints(game[selectedId ^ 1]);
+    calculatePoints(game[selectedId]) > calculatePoints(game[selectedId ^ 1]);
   return (
     <Dialog.Root
       open={open}
@@ -58,7 +58,7 @@ const ConfirmGoingWetPopOver = ({
         <Dialog.Overlay className="fixed inset-0" />
         <Dialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="m-0 mb-3 text-xl font-medium">
-            Bevestig dat team "{teamName}" nat gaat
+            Bevestig dat team "{teamName}" mache de schwimm schwamm
           </Dialog.Title>
           {couldBeMistake && (
             <div>
