95c3812fe83a73aedebd5299a5f0293349cc76f9
Annes Negmel-Din <annesnour03@gmail.com>
2023-12-10 20:26:30 +0100

    jass: add file

--- new_portfolio/src/pages/PlayJass/PlayJass.constants.ts
+++ new_portfolio/src/pages/PlayJass/PlayJass.constants.ts
@@ -5,4 +5,5 @@ export const NO_GAMES = 16;
 export const AUDIO_SOURCES = [
   "https://dl.sndup.net/tpnd/output.mp3",
   "https://dl.sndup.net/hwg7/flip-shorter.mp3",
+  "https://dl.sndup.net/ktqm/tweemotten.mp3"
 ];
